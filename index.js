c061115d5b8af187cb1cdff647ee0bcd36802917
med <mohammed.bennaceur98@gmail.com>
2023-09-14 14:31:27 +0100

    done

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  return word === [...word].reverse().join("");
 }
 
 /* 
   Add your pseudocode here
+
+  initalize Algotrithme
+  revserse the word
+  if revsersed word equals the word 
+  return true
+  else 
+  return false
+
 */
 
 /*
   Add written explanation of your solution here
+
+  create an array from the string word
+  revserse the array 
+  join the array so it becomes a string again
+  compare between the word and its revserse
 */
 
 // You can run `node index.js` to view these console logs
